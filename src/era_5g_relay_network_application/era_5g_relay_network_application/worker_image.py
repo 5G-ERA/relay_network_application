527d026dd99e20fdc2fda816d5fdf8cc78b27cd7
zdenekm <imaterna@fit.vut.cz>
2023-07-25 13:56:56 +0200

    fix: locking, payload, formatted using black

--- src/era_5g_relay_network_application/era_5g_relay_network_application/worker_image.py
+++ src/era_5g_relay_network_application/era_5g_relay_network_application/worker_image.py
@@ -1,17 +1,9 @@
 import base64
 from queue import Empty, Queue
-import time
 import cv2
-import flask_socketio
-import logging
-from threading import Event, Thread
 
 import numpy as np
-import rospy
-from rosbridge_library.internal.message_conversion import populate_instance
-from rosbridge_library.internal import ros_loader
 
-from sensor_msgs.msg import LaserScan
 from cv_bridge import CvBridge
 
 from era_5g_relay_network_application.worker import Worker
@@ -19,9 +11,9 @@ from era_5g_relay_network_application.worker import Worker
 
 class WorkerImage(Worker):
     """
-    Worker object for data processing in standalone variant. Reads 
+    Worker object for data processing in standalone variant. Reads
     data from passed queue, performs detection and returns results using
-    the flask app. 
+    the flask app.
     """
 
     def __init__(self, queue: Queue, topic_name, topic_type, **kw):
@@ -30,14 +22,13 @@ class WorkerImage(Worker):
 
     def get_data(self):
         try:
-            d = self.queue.get(block=True, timeout=1)
-            
+            d, ts = self.queue.get(block=True, timeout=1)
+
             frame = base64.b64decode(d)
             img = cv2.imdecode(np.frombuffer(frame, dtype=np.uint8), cv2.IMREAD_COLOR)
-            return self.bridge.cv2_to_imgmsg(img, encoding='bgr8')
-            # TODO: add timestamp
+            msg = self.bridge.cv2_to_imgmsg(img, encoding="bgr8")
+            msg.header.stamp.secs = int(ts / 10**9)
+            msg.header.stamp.nsecs = ts % 10**9
+            return msg
         except Empty:
-            return None 
-    
-        
-        
+            return None
