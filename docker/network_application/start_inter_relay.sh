06a0321e9107307424ebb77ddb587712fd536721
Michal Kapinus <ikapinus@fit.vutbr.cz>
2024-01-24 14:49:13 +0100

    feat: updated inter_relay docker to be compatible with pants

--- docker/network_application/start_inter_relay.sh
+++ docker/network_application/start_inter_relay.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 
-cd /root/era_5g_relay_network_application/era_5g_relay_network_application
 source /opt/ros/humble/setup.bash
-exec python3 interface_inter.py
+exec python3 -m era_5g_relay_network_application.server_inter
